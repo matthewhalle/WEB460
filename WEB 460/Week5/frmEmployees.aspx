e5e48e07ea454a7447ca1d10154ee3012c30704f
Matthew Halle <halle.matthew@gmail.com>
2013-08-05 09:03:33 -0700

    week 5 update
    use with XML datasets

---
+++ WEB 460/Week5/frmEmployees.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmEmployees.aspx.cs" Inherits="WEB_460.Week5.frmEmployees" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Name "></asp:Label>
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+    
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Age "></asp:Label>
+    
+        <asp:TextBox ID="txtAge" runat="server"></asp:TextBox>
+    
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Gender "></asp:Label>
+        <asp:DropDownList ID="DropGender" runat="server">
+            <asp:ListItem>M</asp:ListItem>
+            <asp:ListItem>F</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Salary "></asp:Label>
+        <asp:TextBox ID="txtSalary" runat="server"></asp:TextBox>
+        <br />
+    
+        <br />
+        <asp:Button ID="btnAddEmployee" runat="server" Text="Add Employee" 
+            onclick="btnAddEmployee_Click" />
+    
+        <br />
+    
+        <br />
+        <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+        <br />
+        <br />
+        <asp:GridView ID="gvEmployees" runat="server">
+        </asp:GridView>
+    
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WEB 460/Week5/frmEmployees.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WEB_460.Week5
+{
+    public partial class frmEmployees : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (!Page.IsPostBack)
+                {
+                    GetAllEmployees();
+                }
+                else
+                {
+                    DataSet employeeSet = null;
+
+                    //rebind the GridView object with the cached copy of the dataset
+
+                    employeeSet = Session["EmployeesDataset"] as DataSet;
+
+                    gvEmployees.DataSource = employeeSet;
+
+                    gvEmployees.DataBind();
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error has happened. " + ex.Message;
+
+            }
+
+        }
+        public void GetAllEmployees()
+        {
+            DataSet myEmployeeSet = new DataSet();
+
+            //read the xml file to the dataset object
+
+            myEmployeeSet.ReadXml(Server.MapPath("employees.xml"));
+
+            gvEmployees.DataSource = myEmployeeSet;
+
+            gvEmployees.DataBind();
+
+            //keep the dataset in the Session object to insure its persistence between postbacks so it is used to
+
+            //refresh the ViewGrid between postbacks
+
+            Session.Add("EmployeesDataset", myEmployeeSet);
+
+        }
+        public void AddEmployee(string name, string age, string gender, string salary)
+        {
+            DataSet EmployeeSet = default(DataSet);
+
+            DataRow NewEmployeeRow = default(DataRow);
+
+            //get the employees dataset from the cach, and add the new employee to it
+
+            EmployeeSet = Session["EmployeesDataset"] as DataSet;
+            
+
+            NewEmployeeRow = EmployeeSet.Tables[0].NewRow();
+            // add new row to the dataset's table
+
+            //assign the fields to the columns of the newly created row
+
+            NewEmployeeRow[0] = name;
+
+            NewEmployeeRow[1] = age;
+
+            NewEmployeeRow[2] = gender;
+
+            NewEmployeeRow[3] = salary;
+
+            EmployeeSet.Tables[0].Rows.Add(NewEmployeeRow);
+            //add it
+
+            EmployeeSet.WriteXml(Server.MapPath("employees.xml"));
+
+            //rebind the ViewGrid to view the newly added employee record
+
+            gvEmployees.DataSource = EmployeeSet;
+
+            gvEmployees.DataBind();
+
+        }
+
+        protected void btnAddEmployee_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AddEmployee(txtName.Text, txtAge.Text, DropGender.SelectedValue, txtSalary.Text);
+
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error has happened. " + ex.Message;
+
+            }
+        }
+
+
+    }
+}
